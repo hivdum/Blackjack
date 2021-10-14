e77423a7f31888bb28b30a5ecdbff23e96a96339
Hivdum <59579911+hivdum@users.noreply.github.com>
2021-10-14 13:50:07 -0400

    Update blackjack.java

--- blackjack.java
+++ blackjack.java
@@ -9,8 +9,12 @@ class blackjack{
 
      public static void main (String str[]) throws IOException {
           Scanner scan = new Scanner(System.in);
-          double card1 = (1 + (Math.random()*10));
-          double card2 = (1 + (Math.random()*10));
+          double card1 = (1 + (Math.random()*13));
+          double card2 = (1 + (Math.random()*13));
+          if (card1 == 11 || card1 == 12)
+               card1 == 10;
+          if (card2 == 11 || card2 == 12)
+               card2 == 10;
           int car1 = (int)card1;
           int car2 = (int)card2;
           System.out.println("Your current hand is " + car1 + " and " + car2 + " = " + (car1 + car2));
