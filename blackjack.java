214c0649b66aba46aa27104e16f87d59edc2069e
Hivdum <59579911+hivdum@users.noreply.github.com>
2021-10-26 15:31:53 -0400

    Add files via upload

--- blackjack.java
+++ blackjack.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 import static java.lang.System.*;
 
 import java.util.Scanner;
@@ -10,33 +9,26 @@ class blackjack{
 
      public static void main (String str[]) throws IOException {
           Scanner scan = new Scanner(System.in);
-          boolean play = true;
-          double card1 = (1 + (Math.random()*13));
-          double card2 = (1 + (Math.random()*13));
-          if (card1 > 10)
+          boolean game = true;
+          //while (game == true);
+             double card1 = (1 + (Math.random()*13));
+             double card2 = (1 + (Math.random()*13));
+             double card3 =  (1 + (Math.random()*13));
+             
+             if (card1 > 10);
                card1 == 10;
-          if (card2 > 10)
+             if (card2 > 10);
                card2 == 10;
-          int car1 = (int)card1;
-          int car2 = (int)card2;
-          int total = car1 + car2;
-          
-          System.out.println("Would you like another card? Inputs: Yes or No");
-          String input = Scanner.nextLine();
-          if( input.toLowerCase().equals("no") || input.toLowerCase().equals("no ") )
-              play = false;
-          
-          ArrayList<Integer> cards = {car1, car2};
-          
-          while(total < 21 && play == true)
-          {
-               System.out.println("Your current hand is " + cards + " = " + (total));
-               System.out.println("Would you like another card? Inputs: Yes or No");
-               String input = Scanner.nextLine();
-               if( input.toLowerCase().equals("no") || input.toLowerCase().equals("no ") )
-                    play = false;
-               
-          }
+             if (card3 > 10);
+               card3 == 10:
+             
+             int car1 = (int)card1;
+             int car2 = (int)card2;
+             int car3 = (int)card3;
+
+             System.out.println("Your current hand is " + car1 + " and " + car2 + " = " + (car1 + car2));
+             System.out.println("Would you like another card? Inputs: Yes or No");
+             
                               
      }
 
